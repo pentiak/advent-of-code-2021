33bea8005caa3e43c29a6e273252950ba8951462
Oskar Majchrzak <oskar_majchrzak@epam.com>
2021-12-02 23:25:48 +0100

    Tests

---
+++ src/test/groovy/com/adventofcode/common/AdventTest.groovy
@@ -0,0 +1,31 @@
+package com.adventofcode.common
+
+import spock.lang.Specification
+
+class AdventTest extends Specification {
+
+    def "Should give correct results"() {
+        given:
+        def exampleDay = Class.forName("com.adventofcode.day" + day + ".Day" + day)
+                .getConstructor(String).newInstance("day" + day + "/example.txt") as Day
+        def inputDay = Class.forName("com.adventofcode.day" + day + ".Day" + day)
+                .getConstructor(String).newInstance("day" + day + "/input.txt") as Day
+
+        expect:
+        if (type == 'example') {
+            exampleDay.part1() == part1
+            exampleDay.part2() == part2
+        }
+        if (type == 'input') {
+            inputDay.part1() == part1
+            inputDay.part2() == part2
+        }
+
+        where:
+        day | type      | part1   | part2
+        1   | 'example' | 7       | 5
+        1   | 'input'   | 1754    | 1789
+        2   | 'example' | 150     | 900
+        2   | 'input'   | 1488669 | 1176514794
+    }
+}
