2608d97a92a9c8432651b77b5f1f434dc95497b2
Oskar Majchrzak <oskar_majchrzak@epam.com>
2021-12-02 21:50:19 +0100

    Day 1 cleanup

--- src/main/java/com/adventofcode/day1/Day1.java
+++ src/main/java/com/adventofcode/day1/Day1.java
@@ -3,42 +3,26 @@ package com.adventofcode.day1;
 import com.adventofcode.utils.InputUtils;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.stream.Collector;
 
+@Log4j2
 public class Day1 {
     public static void main(String[] args) {
-        ResultContainer increases = InputUtils.inputIntStream("day1/input.txt")
-                .collect(ResultContainer::new, ResultContainer::compareValue, (r1, r2) -> {
+        log.info(InputUtils.inputLines("day1/input.txt").map(Integer::valueOf).collect(Collector.<Integer, int[], Integer>of(
+                () -> new int[]{Integer.MAX_VALUE, 0}, (r, v) -> {
+                    if (v > r[0]) r[1]++;
+                    r[0] = v;
+                }, (r1, r2) -> {
                     throw new IllegalStateException();
-                });
-        System.out.println(increases.increases);
+                }, r -> r[1])));
+
         SlidingResultContainer slidingIncreases = InputUtils.inputIntStream("day1/input.txt")
                 .collect(SlidingResultContainer::new, SlidingResultContainer::compareValue, (r1, r2) -> {
                     throw new IllegalStateException();
                 });
-        System.out.println(slidingIncreases.increases);
-
-        int[] result = InputUtils.inputIntStream("day1/input.txt")
-                .collect(() -> new int[]{Integer.MAX_VALUE, 0},
-                        (r, v) -> { if (v > r[0]) r[1]++; r[0] = v; },
-                        (r1, r2) -> { throw new IllegalStateException(); });
-        System.out.println(result[1]);
-
-        System.out.println(InputUtils.inputLines("day1/input.txt").map(Integer::valueOf).collect(Collector.<Integer, int[], Integer>of(
-                () -> new int[]{Integer.MAX_VALUE, 0},(r, v) -> {if (v > r[0]) r[1]++;r[0] = v;},(r1, r2) -> {throw new IllegalStateException();},r -> r[1])));
-    }
-
-    private static class ResultContainer {
-        private int previousValue = -1;
-        private int increases = 0;
-
-        public void compareValue(int value) {
-            if (previousValue != -1 && value > previousValue) {
-                increases++;
-            }
-            previousValue = value;
-        }
+        log.info(slidingIncreases.increases);
     }
 
     private static class SlidingResultContainer {
@@ -59,12 +43,12 @@ public class Day1 {
         }
 
         private void checkIncrease() {
-            int firstWindow = 0;
-            int secondWindow = 0;
-            for (int i = 0; i < 3; i++) {
+            var firstWindow = 0;
+            var secondWindow = 0;
+            for (var i = 0; i < 3; i++) {
                 firstWindow += window.getInt(i);
             }
-            for (int i = 1; i < 4; i++) {
+            for (var i = 1; i < 4; i++) {
                 secondWindow += window.getInt(i);
             }
             if (secondWindow > firstWindow) {
