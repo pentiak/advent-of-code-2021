72b44a67dd2f77600f156a931dcf3de75263c66c
Oskar Majchrzak <oskar.majchrzak@partners.fnality.org>
2021-12-06 12:43:19 +0100

    Day6 refactor to array

--- src/main/java/com/adventofcode/day6/Day6.java
+++ src/main/java/com/adventofcode/day6/Day6.java
@@ -2,8 +2,6 @@ package com.adventofcode.day6;
 
 import com.adventofcode.common.AbstractAdventDay;
 import com.adventofcode.utils.InputUtils;
-import it.unimi.dsi.fastutil.ints.Int2LongLinkedOpenHashMap;
-import it.unimi.dsi.fastutil.ints.Int2LongMap;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.Arrays;
@@ -13,42 +11,40 @@ public class Day6 extends AbstractAdventDay {
 
     @Override
     public Object part1() {
-        Int2LongMap fishes = getInitialState();
+        long[] fishes = getInitialState();
         return countAfterDays(fishes, 80);
     }
 
     @Override
     public Object part2() {
-        Int2LongMap fishes = getInitialState();
+        long[] fishes = getInitialState();
         return countAfterDays(fishes, 256);
     }
 
-    private long countAfterDays(Int2LongMap fishes, int days) {
+    private long countAfterDays(long[] fishes, int days) {
         evolve(fishes, days);
-        return fishes.values().longStream().sum();
+        return Arrays.stream(fishes).sum();
     }
 
-    private void evolve(Int2LongMap fishesMap, int days) {
-        log.trace("Initial state: {}", fishesMap);
+    private void evolve(long[] fishes, int days) {
+        log.trace("Initial state: {}", fishes);
 
         for (int day = 0; day < days; day++) {
-            long reproductiveFishes = fishesMap.get(0);
-            for (int i = 1; i <= 8; i++) {
-                fishesMap.put(i - 1, fishesMap.get(i));
-            }
-            fishesMap.put(8, reproductiveFishes);//new borns
-            fishesMap.put(6, fishesMap.get(6) + reproductiveFishes);//new cycle
-            log.trace("After {} day: {}", day + 1, fishesMap);
+            long reproductiveFishes = fishes[0];
+            System.arraycopy(fishes, 1, fishes, 0, fishes.length - 1);
+            fishes[8] = reproductiveFishes;//new borns
+            fishes[6] = fishes[6] + reproductiveFishes;//new cycle
+            log.trace("After {} day: {}", day + 1, fishes);
         }
     }
 
-    private Int2LongMap getInitialState() {
-        Int2LongMap fishes = new Int2LongLinkedOpenHashMap();
+    private long[] getInitialState() {
+        long[] fishes = new long[9];
         InputUtils.inputLines(inputPath)
                 .map(l -> l.split(","))
                 .flatMap(Arrays::stream)
                 .mapToInt(Integer::parseInt)
-                .forEach(i -> fishes.mergeLong(i, 1, Long::sum));
+                .forEach(i -> fishes[i]++);
         return fishes;
     }
 }
