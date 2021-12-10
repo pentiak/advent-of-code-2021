a2c49b27eb0a982124baab1da1ff758305f4b886
Oskar Majchrzak <oskar_majchrzak@epam.com>
2021-12-11 00:51:44 +0100

    Day10 simplified

--- src/main/java/com/adventofcode/day10/Day10.java
+++ src/main/java/com/adventofcode/day10/Day10.java
@@ -2,20 +2,33 @@ package com.adventofcode.day10;
 
 import com.adventofcode.common.AbstractAdventDay;
 import com.adventofcode.utils.InputUtils;
-import it.unimi.dsi.fastutil.chars.CharArrayList;
-import it.unimi.dsi.fastutil.chars.CharStack;
+import it.unimi.dsi.fastutil.chars.*;
 import it.unimi.dsi.fastutil.longs.LongArrayList;
 import it.unimi.dsi.fastutil.longs.LongComparators;
 import it.unimi.dsi.fastutil.longs.LongList;
 
 public class Day10 extends AbstractAdventDay {
 
+    private final Char2CharMap chunkMap = new Char2CharArrayMap(new char[]{'(', '[', '{', '<'}, new char[]{')', ']', '}', '>'});
+    private final Char2IntMap autocompleteScoreMap = new Char2IntArrayMap(new char[]{')', ']', '}', '>'}, new int[]{1, 2, 3, 4});
+    private final Char2IntMap errorScoreMap = new Char2IntArrayMap(new char[]{')', ']', '}', '>'}, new int[]{3, 57, 1197, 25137});
+
     @Override
     public Object part1() {
         char[][] navigationSubsystem = InputUtils.inputCharArray(inputPath);
         int sum = 0;
         for (char[] navigationLine : navigationSubsystem) {
-            sum += getSyntaxErrorScore(navigationLine);
+            CharStack stack = new CharArrayList(navigationLine.length);
+            for (char c : navigationLine) {
+                if (chunkMap.containsKey(c)) {
+                    stack.push(chunkMap.get(c));
+                } else if (c == stack.topChar()) {
+                    stack.popChar();
+                } else {
+                    sum += errorScoreMap.get(c);
+                    break;
+                }
+            }
         }
         return sum;
     }
@@ -25,88 +38,28 @@ public class Day10 extends AbstractAdventDay {
         char[][] navigationSubsystem = InputUtils.inputCharArray(inputPath);
         LongList autoCompleteScores = new LongArrayList(navigationSubsystem.length);
         for (char[] navigationLine : navigationSubsystem) {
-            if (getSyntaxErrorScore(navigationLine) != 0) {
+            CharStack stack = new CharArrayList(navigationLine.length);
+            boolean error = false;
+            for (char c : navigationLine) {
+                if (chunkMap.containsKey(c)) {
+                    stack.push(chunkMap.get(c));
+                } else if (c == stack.topChar()) {
+                    stack.popChar();
+                } else {
+                    error = true;
+                    break;
+                }
+            }
+            if (error) {
                 continue;
             }
-            autoCompleteScores.add(getAutoCompleteScore(navigationLine));
+            long value = 0;
+            while (!stack.isEmpty()) {
+                value = value * 5 + autocompleteScoreMap.get(stack.popChar());
+            }
+            autoCompleteScores.add(value);
         }
-        return pickWinner(autoCompleteScores);
-    }
-
-    private long pickWinner(LongList autoCompleteScores) {
         autoCompleteScores.sort(LongComparators.NATURAL_COMPARATOR);
         return autoCompleteScores.getLong(autoCompleteScores.size() / 2);
-    }
-
-    private long getAutoCompleteScore(char[] line) {
-        long score = 0;
-        CharStack stack = new CharArrayList(line.length);
-        for (char c : line) {
-            if (isChunkBeginning(c)) {
-                stack.push(c);
-            } else {
-                stack.popChar();
-            }
-        }
-        while (!stack.isEmpty()) {
-            score = score * 5 + getAutoCompleteValue(stack.popChar());
-        }
-        return score;
-    }
-
-    private int getAutoCompleteValue(char c) {
-        return switch (c) {
-            case '(' -> 1;
-            case '[' -> 2;
-            case '{' -> 3;
-            case '<' -> 4;
-            default -> throw new IllegalStateException("Unexpected value: " + c);
-        };
-    }
-
-    private int getSyntaxErrorScore(char[] line) {
-        CharStack stack = new CharArrayList(line.length);
-        for (char c : line) {
-            if (!isLegal(c, stack)) {
-                return getErrorValue(c);
-            }
-        }
-        return 0;
-    }
-
-    private int getErrorValue(char c) {
-        return switch (c) {
-            case ')' -> 3;
-            case ']' -> 57;
-            case '}' -> 1197;
-            case '>' -> 25137;
-            default -> throw new IllegalStateException("Unexpected value: " + c);
-        };
-    }
-
-    private boolean isLegal(char c, CharStack stack) {
-        if (isChunkBeginning(c)) {
-            stack.push(c);
-            return true;
-        } else if (isExpectedChunkEnd(c, stack)) {
-            stack.popChar();
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean isExpectedChunkEnd(char c, CharStack stack) {
-        return c == switch (stack.topChar()) {
-            case '(' -> ')';
-            case '[' -> ']';
-            case '{' -> '}';
-            case '<' -> '>';
-            default -> throw new IllegalStateException("Unexpected value: " + stack.topChar());
-        };
-    }
-
-    private boolean isChunkBeginning(char c) {
-        return c == '(' || c == '[' || c == '{' || c == '<';
     }
 }
